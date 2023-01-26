3a4bad0f020114d1c2a9bbf2bd11e9886f5e6221
Bilal Bakht Ahmad <bilalba@users.noreply.github.com>
2023-01-26 11:55:08 -0800

    oxford comma
    Co-authored-by: David Farr <david_farr@intuit.com>

--- controllers/eventbus/validate.go
+++ controllers/eventbus/validate.go
@@ -9,7 +9,7 @@ import (
 // ValidateEventBus accepts an EventBus and performs validation against it
 func ValidateEventBus(eb *v1alpha1.EventBus) error {
 	if eb.Spec.NATS == nil && eb.Spec.JetStream == nil && eb.Spec.Kafka == nil {
-		return fmt.Errorf("invalid spec: either \"nats\", \"jetstream\" or \"kafka\" needs to be specified")
+		return fmt.Errorf("invalid spec: either \"nats\", \"jetstream\", or \"kafka\" needs to be specified")
 	}
 	if x := eb.Spec.NATS; x != nil {
 		if x.Native != nil && x.Exotic != nil {
