c1015c2931dd680b66988767e0d5d44f14f681b3
David Farr <david_farr@intuit.com>
2023-01-30 23:54:11 -0800

    Implement kafka sensor evaluate
    Signed-off-by: David Farr <david_farr@intuit.com>

--- eventbus/kafka/sensor/trigger_handler.go
+++ eventbus/kafka/sensor/trigger_handler.go
@@ -2,9 +2,12 @@ package kafka
 
 import (
 	"encoding/json"
+	"strings"
 
+	"github.com/Knetic/govaluate"
 	"github.com/argoproj/argo-events/eventbus/common"
 	cloudevents "github.com/cloudevents/sdk-go/v2"
+	"go.uber.org/zap"
 )
 
 type KafkaTriggerHandler interface {
@@ -37,15 +40,15 @@ func (c *KafkaTriggerConnection) DependsOn(event *cloudevents.Event) (string, bo
 	return "", false
 }
 
-func (h *KafkaTriggerConnection) Transform(depName string, event cloudevents.Event) (*cloudevents.Event, error) {
-	return h.transform(depName, event)
+func (c *KafkaTriggerConnection) Transform(depName string, event cloudevents.Event) (*cloudevents.Event, error) {
+	return c.transform(depName, event)
 }
 
-func (h *KafkaTriggerConnection) Filter(depName string, event cloudevents.Event) bool {
-	return h.filter(depName, event)
+func (c *KafkaTriggerConnection) Filter(depName string, event cloudevents.Event) bool {
+	return c.filter(depName, event)
 }
 
-func (h *KafkaTriggerConnection) Update(event *cloudevents.Event, partition int32, offset int64) (*cloudevents.Event, error) {
+func (c *KafkaTriggerConnection) Update(event *cloudevents.Event, partition int32, offset int64) (*cloudevents.Event, error) {
 	found := false
 	eventWithPartitionAndOffset := &eventWithPartitionAndOffset{
 		Event:     event,
@@ -53,37 +56,42 @@ func (h *KafkaTriggerConnection) Update(event *cloudevents.Event, partition int3
 		offset:    offset,
 	}
 
-	for i := 0; i < len(h.events); i++ {
-		if h.events[i].Source() == event.Source() && h.events[i].Subject() == event.Subject() {
-			h.events[i] = eventWithPartitionAndOffset
+	for i := 0; i < len(c.events); i++ {
+		if c.events[i].Source() == event.Source() && c.events[i].Subject() == event.Subject() {
+			c.events[i] = eventWithPartitionAndOffset
 			found = true
 			break
 		}
 	}
 
 	if !found {
-		h.events = append(h.events, eventWithPartitionAndOffset)
+		c.events = append(c.events, eventWithPartitionAndOffset)
 	}
 
-	if h.satisfied() {
+	satisfied, err := c.satisfied()
+	if err != nil {
+		return nil, err
+	}
+
+	if satisfied == true {
 		action := cloudevents.NewEvent()
 		action.SetID(event.ID()) // use id of last event
-		action.SetSource(h.sensorName)
-		action.SetSubject(h.triggerName)
+		action.SetSource(c.sensorName)
+		action.SetSubject(c.triggerName)
 
-		if err := action.SetData(cloudevents.ApplicationJSON, h.events); err != nil {
+		if err := action.SetData(cloudevents.ApplicationJSON, c.events); err != nil {
 			return nil, err
 		}
 
-		h.reset()
+		c.reset()
 		return &action, nil
 	}
 
 	return nil, nil
 }
 
-func (h *KafkaTriggerConnection) Offset(partition int32, offset int64) int64 {
-	for _, event := range h.events {
+func (c *KafkaTriggerConnection) Offset(partition int32, offset int64) int64 {
+	for _, event := range c.events {
 		if partition == event.partition && offset > event.offset {
 			offset = event.offset
 		}
@@ -92,7 +100,7 @@ func (h *KafkaTriggerConnection) Offset(partition int32, offset int64) int64 {
 	return offset
 }
 
-func (h *KafkaTriggerConnection) Action(event cloudevents.Event) error {
+func (c *KafkaTriggerConnection) Action(event cloudevents.Event) error {
 	var events []*cloudevents.Event
 	if err := json.Unmarshal(event.Data(), &events); err != nil {
 		return err
@@ -100,23 +108,47 @@ func (h *KafkaTriggerConnection) Action(event cloudevents.Event) error {
 
 	eventMap := map[string]cloudevents.Event{}
 	for _, event := range events {
-		if depName, ok := h.DependsOn(event); ok {
+		if depName, ok := c.DependsOn(event); ok {
 			eventMap[depName] = *event
 		}
 	}
 
 	// todo: implement at least once / at most once
 
-	h.action(eventMap)
+	c.action(eventMap)
 
 	return nil
 }
 
-// todo: implement dep expression
-func (h *KafkaTriggerConnection) satisfied() bool {
-	return len(h.events) == len(h.dependencies)
+func (c *KafkaTriggerConnection) satisfied() (interface{}, error) {
+	expr, err := govaluate.NewEvaluableExpression(strings.ReplaceAll(c.depExpression, "-", "\\-"))
+	if err != nil {
+		return false, err
+	}
+
+	parameters := Parameters{}
+	for _, event := range c.events {
+		// todo: make more efficient
+		if depName, ok := c.DependsOn(event.Event); ok {
+			parameters[depName] = true
+		}
+	}
+
+	c.Logger.Infow("Evaluating", zap.String("expr", c.depExpression), zap.Any("parameters", parameters))
+
+	return expr.Eval(parameters)
 }
 
-func (h *KafkaTriggerConnection) reset() {
-	h.events = []*eventWithPartitionAndOffset{}
+func (c *KafkaTriggerConnection) reset() {
+	c.events = []*eventWithPartitionAndOffset{}
+}
+
+type Parameters map[string]bool
+
+func (p Parameters) Get(name string) (interface{}, error) {
+	if parameter, ok := p[name]; ok {
+		return parameter, nil
+	}
+
+	return false, nil
 }
