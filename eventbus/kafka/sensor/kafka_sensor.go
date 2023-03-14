22dcbdaced447c509cf15c4dce41dc7e808daaa0
David Farr <david_farr@intuit.com>
2023-03-13 20:01:07 -0700

    wip

--- eventbus/kafka/sensor/kafka_sensor.go
+++ eventbus/kafka/sensor/kafka_sensor.go
@@ -258,7 +258,7 @@ func (s *KafkaSensor) Event(msg *sarama.ConsumerMessage) ([]*sarama.ProducerMess
 		}
 
 		if !trigger.Filter(trigger.depName, event) {
-			s.Logger.Debug("Filter condition satisfied, skipping")
+			// s.Logger.Debug("Filter condition satisfied, skipping")
 			continue
 		}
 
